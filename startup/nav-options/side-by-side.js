3ef76fe9bec95268babd44640e99aa5fb1a88cd9
kippered <jstjames@strategicfactory.com>
2025-12-18 14:25:07 -0400

    Adding more features
    Filling out the side-by-side layout script, created bulk of nav CSS

--- startup/nav-options/side-by-side.js
+++ startup/nav-options/side-by-side.js
@@ -1,17 +1,33 @@
-$("#header_logo img").appendTo("#header-wrapper");
-$("#header-wrapper").append(`<nav><ul class="nav">`);
+/*
+    TODO: currently cannot handle tabs with more than 1 level of nesting because I haven't yet had a site to test on
+*/
 
+$("#header-wrapper").after(`<header><nav><ul class="nav">`);
+$("#header_logo img").prependTo("header nav");
+
+// For each tab in the original tab container:
 $(`#tab_responsive_tabs [id^="tab_"]`).each(function() {
     // If the tab has no sub-items, add it to the nav as a link.
-    // If it does have sub-items, add it to the nav as a hoverable container.
-    if ( $(this).find("dfn").length == 1 ) {
+    if ( $(this).find("dfn").length === 1 ) {
         $(".nav").append(`<li>${ $(this).find("dfn").first().html() }</li>`);
     }
-    else {
-        let sub = $(`<ul class="nav__tab-item">`);
-        let menu = $(`<li class="nav__tab-menu"><a><span>${ $(this).find("dfn").first().text() }</span><i class="fa fa-sort-desc"></i></a></li>`)
 
+    // If it does have sub-items, add it to the nav as a hoverable container.
+    else {
+        let menu = $(`
+            <li class="nav__tab-menu">
+                <a>
+                    <span>${ $(this).find("dfn").first().text() }</span>
+                    <i class="fa fa-sort-desc"></i>
+                </a>
+            </li>
+        `);
+
+        let sub = $(`<ul class="nav__tab-sub">`);
+
+        // For each child tab:
         $(this).children().find('dfn').each(function() {
+            // If it does not have any children, add it to the parent menu as a link.
             if ( $(this).is(":last-child") ) {
                 sub.append(`<li>${ $(this).html() }</li>`);
             }
@@ -19,15 +35,27 @@ $(`#tab_responsive_tabs [id^="tab_"]`).each(function() {
 
         menu.append(sub);
         $(".nav").append(menu);
-        // Bad code; there's no reason for this to use a tab ID instead of just looping
-        /*$(".nav").append(`
-            
-                <a>
-                    <span>${ $(this).find("dfn").first().text() }</span>
-                    <i class="fa fa-sort-desc"></i>
-                </a>
-                <ul class="tab-submenu" id="nav-${tabID}"></ul>
-            </li>
-        `);*/
     }
 });
+
+// Add the basket link from the original header.
+$(".nav").append(`<li><a href="basket_view.cgi"><i class="fa fa-shopping-basket"></i><span>${ $(".cart_num_items").text() }</span></a></li>`);
+
+// Create a new submenu with the profile and logout links.
+$(".nav").append(`
+    <li class="nav__tab-menu">
+        <a>
+            <span>${ $("#logout_userid").text() }</span>
+            <i class="fa fa-sort-desc"></i>
+        </a>
+        <ul class="nav__tab-sub">
+            <li><a href="profile.cgi?force_view=1">My Profile</a></li>
+            <li><a id="submit_logout" href="logout.cgi">Logout</a></li>
+        </ul>
+    </li>
+`);
+
+// If user is not viewing the catalog page, insert a link to the catalog.
+if ( !$(".select-items").length ) {
+    $(".nav").prepend(`<li><a href="catalog.cgi">Back to Catalog</a></li>`);
+}
